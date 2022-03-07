13a7bd2b9618cc3ecf2a76fb922d35599b9024f4
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-06 23:13:43 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Repository:  MediaLibrary
+// Author:      Jeff Grissom
+// Version:     1.xx
+
+using System;
+using NLog.Web;
+using System.IO;
+
+namespace MediaLibrary
+{
+    class Program
+    {
+        // create static instance of Logger
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+
+            logger.Info("Program started");
+
+            
+
+            string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+            logger.Info(scrubbedFile);
+            MovieFile movieFile = new MovieFile(scrubbedFile);
+
+            logger.Info("Program ended");
+        }
+    }
+}
