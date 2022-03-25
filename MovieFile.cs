9e449bbaad2e1580cde62f7f6f36b29b7241f7b2
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-25 15:32:20 -0500

    Add the user menu

--- MovieFile.cs
+++ MovieFile.cs
@@ -1,113 +1,148 @@
-using NLog.Web;
 using System;
-using System.Collections.Generic;
 using System.IO;
+using NLog.Web;
 using System.Linq;
+using System.Collections.Generic;
+
+
 
 namespace MediaLibrary
 {
     public class MovieFile
     {
-        // public property
         public string filePath { get; set; }
         public List<Movie> Movies { get; set; }
+    
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
-
-        // constructor is a special method that is invoked
-        // when an instance of a class is created
-        public MovieFile(string movieFilePath)
+        List<Movie> movies = new List<Movie>();
+        string csvHeader = "";
+        string movieFile;
+        public MovieFile(string movieFile)
         {
-            filePath = movieFilePath;
-            Movies = new List<Movie>();
+            this.movieFile = movieFile;
+            this.loadMovies();
+        }
 
-            // to populate the list with data, read from the data file
+        public void addMovieToList(Movie movie) {
+            this.movies.Add(movie);
+        }
+
+         public void loadMovies() {
             try
             {
-                StreamReader sr = new StreamReader(filePath);
-                while (!sr.EndOfStream)
-                {
-                    // create instance of Movie class
-                    Movie movie = new Movie();
-                    string line = sr.ReadLine();
-                    // first look for quote(") in string
-                    // this indicates a comma(,) in movie title
-                    int idx = line.IndexOf('"');
-                    if (idx == -1)
+                StreamReader sr1 = new StreamReader(this.movieFile);
+                if (File.Exists(this.movieFile)) {
+                    while (!sr1.EndOfStream)
                     {
-                        // no quote = no comma in movie title
-                        // movie details are separated with comma(,)
-                        string[] movieDetails = line.Split(',');
-                        movie.mediaId = UInt64.Parse(movieDetails[0]);
-                        movie.title = movieDetails[1];
-                        movie.genres = movieDetails[2].Split('|').ToList();
-                        movie.director = movieDetails[3];
-                        movie.runningTime = TimeSpan.Parse(movieDetails[4]);
+                        Movie movie = new Movie();
+                        string line = sr1.ReadLine();
+                        int idx = line.IndexOf('"');
+                        
+                        if (idx == -1)
+                        {
+                            // no quote = no comma or quote in movie title
+                            // movie details are separated with comma(,)
+                            string[] movieDetails = line.Split(',');
+                            movie.mediaId = UInt64.Parse(movieDetails[0]);
+                            movie.title = movieDetails[1];
+
+                            String[] genreData = movieDetails[2].Split("|");
+                            movie.genres = genreData.ToList<string>();
+                            movie.director = movieDetails.Length > 3 ? movieDetails[3] : "unassigned";
+                            movie.runningTime = movieDetails.Length > 4 ? TimeSpan.Parse(movieDetails[4]) : new TimeSpan(0);
+                        }
+                        else
+                        {
+                            // quote = comma or quotes in movie title
+                            // extract the movieId
+                            movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
+                            // remove movieId and first comma from string
+                            line = line.Substring(idx);
+                            // find the last quote
+                            idx = line.LastIndexOf('"');
+                            // extract title
+                            movie.title = line.Substring(0, idx + 1);
+                            // remove title and next comma from the string
+                            line = line.Substring(idx + 2);
+                            // split the remaining string based on commas
+                            string[] details = line.Split(',');
+                            // the first item in the array should be genres 
+                            String[] genreData = details[0].Split("|");
+                            movie.genres = genreData.ToList<string>();
+                            // if there is another item in the array it should be director
+                            movie.director = details.Length > 1 ? details[1] : "unassigned";
+                            // if there is another item in the array it should be run time
+                            movie.runningTime = details.Length > 2 ? TimeSpan.Parse(details[2]) : new TimeSpan(0);
+                        }
+
+                        this.addMovieToList(movie);
+                        
                     }
-                    else
-                    {
-                        // quote = comma or quotes in movie title
-                        // extract the movieId
-                        movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
-                        // remove movieId and first comma from string
-                        line = line.Substring(idx);
-                        // find the last quote
-                        idx = line.LastIndexOf('"');
-                        // extract title
-                        movie.title = line.Substring(0, idx + 1);
-                        // remove title and next comma from the string
-                        line = line.Substring(idx + 2);
-                        // split the remaining string based on commas
-                        string[] details = line.Split(',');
-                        // the first item in the array should be genres 
-                        movie.genres = details[0].Split('|').ToList();
-                        // if there is another item in the array it should be director
-                        movie.director = details[1];
-                        // if there is another item in the array it should be run time
-                        movie.runningTime = TimeSpan.Parse(details[2]);
-                    }
-                    Movies.Add(movie);
+                } else {
+                    logger.Error("File does not exist: " + this.movieFile);
                 }
-                // close file when done
-                sr.Close();
-                logger.Info("Movies in file {Count}", Movies.Count);
+                    
             }
             catch (Exception ex)
             {
-                logger.Error(ex.Message);
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(ex.Message);
+                logger.Error("There has been an Exception " + ex.Message);
             }
         }
 
-        // public method
-        public bool isUniqueTitle(string title)
-        {
-            if (Movies.ConvertAll(m => m.title.ToLower()).Contains(title.ToLower()))
-            {
-                logger.Info("Duplicate movie title {Title}", title);
-                return false;
+        public void printMovies() {
+            foreach (Movie movie in this.movies) {
+                string genres = string.Join(", ", movie.genres);
+                Console.WriteLine($"Id: {movie.mediaId}\nTitle: {movie.title}\nDirector: {movie.director}\nRunning Time: {movie.runningTime}\nGenres: {genres}\n\n");
             }
-            return true;
         }
 
-        public void AddMovie(Movie movie)
-        {
-            try
-            {
-                // first generate movie id
-                movie.mediaId = Movies.Max(m => m.mediaId) + 1;
-                // if title contains a comma, wrap it in quotes
-                string title = movie.title.IndexOf(',') != -1 || movie.title.IndexOf('"') != -1 ? $"\"{movie.title}\"" : movie.title;
-                StreamWriter sw = new StreamWriter(filePath, true);
-                // write movie data to file
-                sw.WriteLine($"{movie.mediaId},{title},{string.Join("|", movie.genres)},{movie.director},{movie.runningTime}");
-                sw.Close();
-                // add movie details to List
-                Movies.Add(movie);
-                // log transaction
-                logger.Info("Media id {Id} added", movie.mediaId);
+        public void userAddMovie() {
+            //ask user for certain things
+            Console.WriteLine("Enter movie title.");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Enter movie director.");
+            string director = Console.ReadLine();
+
+            Console.WriteLine("Enter running time. (hh:mm:ss)");
+            TimeSpan timespan = TimeSpan.Parse(Console.ReadLine());
+            // Ask for genre ( can be multiple so ask if they want to add more)
+            Boolean done = false;
+            List<string> genre = new List<string>();
+            while(!done) {
+                Console.WriteLine("Please enter a genre");
+                genre.Add(Console.ReadLine());
+                Console.WriteLine("would you like to add more? yes/no");
+                string userResponse = Console.ReadLine();
+                // ask user if they want to add more
+                if (userResponse != "yes") {
+                    done = true;
+                }
+
             }
-            catch (Exception ex)
+
+            Movie movie = new Movie();
+            movie.mediaId = getNextId();
+            movie.title = title;
+            movie.director = director;
+            movie.genres = genre;
+            movie.runningTime = timespan;
+
+            this.addMovieToList(movie);
+        }
+
+        public UInt64 getNextId() {
+            return this.movies[this.movies.Count-1].mediaId + 1;
+        }
+
+        public void findMovies() {
+            var moviesFound = this.Movies.Where(m => m.title.Contains("(1921)"));
+            Console.WriteLine(moviesFound.Count() + " Matches Found");
+            foreach(Movie m in moviesFound)
             {
-                logger.Error(ex.Message);
+                Console.WriteLine($" - {m.title}");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿// Repository:  MediaLibrary
-// Author:      Jeff Grissom
-// Version:     1.xx
-
-using System;
+﻿using System;
 using NLog.Web;
 using System.IO;
 
@@ -17,12 +13,80 @@ namespace MediaLibrary
 
             logger.Info("Program started");
 
-            
+            // Movie movie = new Movie
+            // {
+            //     mediaId = 123,
+            //     title = "Greatest Movie Ever, The (2020)",
+            //     director = "Jeff Grissom",
+            //     // timespan (hours, minutes, seconds)
+            //     runningTime = new TimeSpan(2, 21, 23),
+            //     genres = { "Comedy", "Romance" }
+            // };
 
-            string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
-            logger.Info(scrubbedFile);
-            MovieFile movieFile = new MovieFile(scrubbedFile);
+            // Console.WriteLine(movie.Display());
 
+            // Album album = new Album
+            // {
+            //     mediaId = 321,
+            //     title = "Greatest Album Ever, The (2020)",
+            //     artist = "Jeff's Awesome Band",
+            //     recordLabel = "Universal Music Group",
+            //     genres = { "Rock" }
+            // };
+            // Console.WriteLine(album.Display());
+
+            // Book book = new Book
+            // {
+            //     mediaId = 111,
+            //     title = "Super Cool Book",
+            //     author = "Jeff Grissom",
+            //     pageCount = 101,
+            //     publisher = "",
+            //     genres = { "Suspense", "Mystery" }
+            // };
+            // Console.WriteLine(book.Display());          
+
+            //  string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+            //  logger.Info(scrubbedFile);
+
+            logger.Info("Movie Manager loaded.");
+            MovieFile manager = new MovieFile("movies.scrubbed.csv");
+
+            try
+            {
+                
+                Boolean done = false;
+
+                while(!done) {
+                    Console.WriteLine("\nWhat would you like to do?");
+                    Console.WriteLine("1 - Add New Movie");
+                    Console.WriteLine("2 - Dispaly All Movies");
+                    Console.WriteLine("3 - Find Movie");
+                    Console.WriteLine("Q - Quit Program");
+
+                    string choice = Console.ReadLine();
+                    logger.Info("Users choice = " + choice);
+
+                    if(choice == "1") {
+                        manager.userAddMovie();
+                    } else if (choice == "2") {
+                        manager.printMovies();
+                    } else if (choice == "3"){
+                        manager.findMovies();
+                        logger.Info("Find Movies");
+                    } else if (choice == "Q" || choice == "q") {
+                        done = true;
+                    } else {
+                        Console.WriteLine("Not a valid choice.");
+                    };
+                }
+                logger.Info("Program Quitting");
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
             logger.Info("Program ended");
         }
     }
